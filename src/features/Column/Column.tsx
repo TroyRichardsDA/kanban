dfcc7909f1ddc67a13eb26d23850b6f4bbb455cb
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-10-14 18:40:25 -0400

    Column base complete / Card started

--- src/features/Column/Column.tsx
+++ src/features/Column/Column.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 import { Task } from "../../models/Task";
+import styles from "./Column.module.scss";
 
 interface Props {
   name: string;
@@ -8,10 +10,22 @@ interface Props {
 
 const Column = (props: Props) => {
   const { name, tasks } = props;
+  const backgroundColor =
+    name === "todo" ? "#49C4E5" : name === "doing" ? "$primary" : "#67E2AE";
 
   return (
     <section>
-      <h2>{name}</h2>
+      <div className={styles.title}>
+        <div style={{ backgroundColor }} className={styles.circle}></div>
+        <h2>
+          {name} {!tasks ? "" : `(${tasks?.length})`}{" "}
+        </h2>
+      </div>
+      <div>
+        {tasks?.map((task) => (
+          <Card />
+        ))}
+      </div>
     </section>
   );
 };
